a87249da3406caccd51fe2127b958e0385157702
rizorko <ms.katyaaa13@mail.ru>
2016-12-08 19:00:49 +0200

    Local Storage + Drag-n-Drop

--- Homework5/scripts/script.js
+++ Homework5/scripts/script.js
@@ -4,6 +4,8 @@ var enterTask = document.getElementById("enterTask");
 enterTask.focus();
 var counter = document.getElementById("counter");
 
+var storage = window.localStorage || null;
+
 function updateTaskSelected() {
     var checkID = this.id.replace("check_", "");
     var task = document.getElementById("text_" + checkID);
@@ -16,11 +18,13 @@ function updateTaskSelected() {
         task.className = "";
     }
     counter.innerText = "You selected " + countChecked + " items.";
+    saveToLS();
 }
 
 function editedItem() {
     document.getElementById("parent_popup").style.display = "block";
     document.getElementById("popup").style.display = "block";
+    document.getElementById("editTask").focus();
     var editID = this.id.replace("edit_", "");
     var oldTask = document.getElementById("text_" + editID).innerHTML;
     document.getElementById("editTask").value = oldTask;
@@ -36,6 +40,7 @@ function editedItem() {
         document.getElementById("text_" + editID).innerText = editedTask;
         document.getElementById("popup").style.display = "none";
         document.getElementById("parent_popup").style.display = "none";
+        saveToLS();
     }, false);
 
     cancelButton.addEventListener("click", function(){
@@ -52,11 +57,12 @@ function removeItem() {
         countChecked--;
     }
     counter.innerText = "You selected " + countChecked + " items.";
-    document.getElementById("li_" + itemID).style.display = "none";
-
+    document.getElementById("li_" + itemID).remove();
+    //document.getElementById("li_" + itemID).style.display = "none";
+    saveToLS();
 }
 
-function addNewTask(todoList, item) {
+function addNewTask(todoList, item, checked) {
     counterID++;
     var task = document.createElement("li");
     task.setAttribute("draggable", true);
@@ -66,29 +72,36 @@ function addNewTask(todoList, item) {
     var checkBox = document.createElement("input");
     checkBox.type = "checkbox";
     checkBox.id = "check_" + counterID;
+    checkBox.checked = checked;
     checkBox.addEventListener("click", updateTaskSelected, false);
 
     var p = document.createElement("p");
     p.id = "text_" + counterID;
     p.innerText = item;
-
-    var editButton = document.createElement("button");
-    editButton.id = "edit_" + counterID;
-    editButton.innerText = "Edit";
+    if(checked) {
+         p.className = "checked";
+    }
 
     var removeButton = document.createElement("button");
     removeButton.id = "remove_" + counterID;
-    removeButton.innerText = "Remove";
+    removeButton.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
 
+    var editButton = document.createElement("button");
+    editButton.id = "edit_" + counterID;
+    editButton.innerHTML = '<i class="fa fa-pencil" aria-hidden="true"></i>';
+
+    
     editButton.addEventListener("click", editedItem, false);
+
     removeButton.addEventListener("click", removeItem, false);
 
     task.appendChild(checkBox);
     task.appendChild(p);
-    task.appendChild(editButton);
     task.appendChild(removeButton);
+    task.appendChild(editButton);
 
     todoList.appendChild(task);
+    saveToLS();
 }
 
 enterTask.onkeyup = function(event) {
@@ -100,7 +113,7 @@ enterTask.onkeyup = function(event) {
             return false;
         }
 
-        addNewTask(document.getElementById("todo-list"), taskItem);
+        addNewTask(document.getElementById("todo-list"), taskItem, false);
         document.getElementById("enterTask").value = "";
     }
 };
@@ -113,7 +126,98 @@ document.getElementById("add").addEventListener("click", function(event) {
             return false;
         }
 
-        addNewTask(document.getElementById("todo-list"), taskItem);
+        addNewTask(document.getElementById("todo-list"), taskItem, false);
         document.getElementById("enterTask").value = "";
 }, false);
 
+
+function saveToLS() {
+    var children = document.getElementById("todo-list").children;
+    var toSave = [];
+    for(var i = 0; i < children.length; i++) {
+        var checked = children[i].querySelector("input[type='checkbox']").checked;
+        var text = children[i].querySelector("p").innerText;
+
+        toSave.push({ text: text, checked: checked });
+    }
+
+    storage.setItem('todo', JSON.stringify(toSave));
+    storage.setItem('todo-checked', countChecked);
+
+    console.log("Saved to LS.");
+}
+
+function loadFromLS() {
+    document.getElementById("todo-list").innerHTML = "";
+    counterID = 0;
+    var amountChecked = 0;
+
+    var got = JSON.parse(storage.getItem('todo'));
+    for(var i = 0; i < got.length; i++) {
+        var item = got[i];
+        if(got[i].checked) { amountChecked++; }
+        addNewTask(document.getElementById("todo-list"), item.text, item.checked);
+    }
+
+    countChecked = amountChecked;
+    counter.innerText = "You selected " + countChecked + " items.";
+
+    console.log("Loaded from LS.");
+}
+
+function sortable(rootEl, onUpdate){
+    var dragEl, nextEl;
+    
+    [].slice.call(rootEl.children).forEach(function (itemEl){
+        itemEl.draggable = true;
+    });
+    
+    function _onDragOver(evt){
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'move';
+       
+        var target = evt.target;
+        if( target && target !== dragEl && target.nodeName == 'LI' ){
+            var rect = target.getBoundingClientRect();
+            var next = (evt.clientY - rect.top)/(rect.bottom - rect.top) > .5;
+            rootEl.insertBefore(dragEl, next && target.nextSibling || target);
+        }
+    }
+    
+    function _onDragEnd(evt){
+        evt.preventDefault();
+       
+        dragEl.classList.remove('ghost');
+        rootEl.removeEventListener('dragover', _onDragOver, false);
+        rootEl.removeEventListener('dragend', _onDragEnd, false);
+
+        if( nextEl !== dragEl.nextSibling ){
+            onUpdate(dragEl);
+        }
+    }
+    
+    rootEl.addEventListener('dragstart', function (evt){
+        dragEl = evt.target;
+        nextEl = dragEl.nextSibling;
+        
+        evt.dataTransfer.effectAllowed = 'move';
+        evt.dataTransfer.setData('Text', dragEl.textContent);
+
+        rootEl.addEventListener('dragover', _onDragOver, false);
+        rootEl.addEventListener('dragend', _onDragEnd, false);
+
+        setTimeout(function (){
+            dragEl.classList.add('ghost');
+        }, 0)
+    }, false);
+}
+
+
+if(storage == null) { alert("Local Storage is not supported."); }
+if(JSON.parse(storage.getItem('todo')).length != 0) {
+    loadFromLS();
+}
+
+sortable( document.getElementById('todo-list'), function (item){
+    saveToLS();
+});
